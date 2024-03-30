cf2f0ad4d12296b6aa22fa8fc193e4504ef70895
Nitin Agrawal <nitinagrawalup@gmail.com>
2024-03-30 20:11:54 +0530

    Updated the Program 1 and Added Program 2

--- src/com/practice/core/java/Program_1.java
+++ src/com/practice/core/java/Program_1.java
@@ -8,8 +8,8 @@ public class Program_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int num1 = 23;
-		float num2 = 52;
-		double num3 = 13;
+		int num2 = 52;
+		int num3 = 13;
 
 		System.out.println(num1 + num2 + num3);
 	}
---
+++ src/com/practice/core/java/Program_2.java
@@ -0,0 +1,25 @@
+package com.practice.core.java;
+
+/*
+ * WAP for showing use of different datatypes
+ */
+public class Program_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte age1 = 23;
+		short age2 = 32;
+		int age3 = 52;
+		float salary1 = 26400.34f;
+		double salary2 = 24799.9444d;
+		long ageDino = 345579983233L;
+		boolean alive = true;
+		char currentEmployee = 'y';
+
+		System.out.println(age1 + " " + age2 + " " + age3 + " " + salary1 + " " + salary2 + " " + ageDino + " " + alive
+				+ " " + currentEmployee);
+
+	}
+
+}
