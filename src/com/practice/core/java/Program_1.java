386bf870e6c629cc43b61538ba4073506473ca55
Nitin Agrawal <nitinagrawalup@gmail.com>
2024-03-30 14:46:31 +0530

    Program 1 commit

---
+++ src/com/practice/core/java/Program_1.java
@@ -0,0 +1,17 @@
+package com.practice.core.java;
+
+/*
+ * WAP to add three number
+ */
+public class Program_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 23;
+		float num2 = 52;
+		double num3 = 13;
+
+		System.out.println(num1 + num2 + num3);
+	}
+
+}
