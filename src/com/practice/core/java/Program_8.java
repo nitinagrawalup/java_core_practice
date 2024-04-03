35b5d8705c45cdbd8b46c7ffcb61cb653497a3ea
Nitin Agrawal <nitinagrawalup@gmail.com>
2024-04-03 22:16:14 +0530

    Commit Program8

---
+++ src/com/practice/core/java/Program_8.java
@@ -0,0 +1,12 @@
+package com.practice.core.java;
+
+public class Program_8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "hARry";
+		String newString = str.toLowerCase();
+		System.out.println(newString);
+	}
+
+}
