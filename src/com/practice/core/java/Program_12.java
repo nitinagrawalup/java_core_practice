7ebbdc7227290318ea190bade3d62b4641c63385
Nitin Agrawal <nitinagrawalup@gmail.com>
2024-04-03 22:25:58 +0530

    Commit Program12

---
+++ src/com/practice/core/java/Program_12.java
@@ -0,0 +1,12 @@
+package com.practice.core.java;
+
+public class Program_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String letter = "Dear Harry, This Java Course is nice.Thanks";
+		String newLetter = "Dear Harry,\n This Java Course is nice.\nThanks";
+		System.out.println(newLetter);
+	}
+
+}
