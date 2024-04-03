c123285c82d1f248ef9cc69f95cf507deffba5d3
Nitin Agrawal <nitinagrawalup@gmail.com>
2024-04-03 22:25:31 +0530

    Commit Program9

---
+++ src/com/practice/core/java/Program_9.java
@@ -0,0 +1,12 @@
+package com.practice.core.java;
+
+public class Program_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "This is Nitin Agrawal";
+		String newString = str.replace(' ', '_');
+		System.out.println(newString);
+	}
+
+}
