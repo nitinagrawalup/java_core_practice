a40853ea7a217e276e9feb3d2614bb25c7ad90d4
Nitin Agrawal <nitinagrawalup@gmail.com>
2024-04-03 22:25:39 +0530

    Commit Program10

---
+++ src/com/practice/core/java/Program_10.java
@@ -0,0 +1,12 @@
+package com.practice.core.java;
+
+public class Program_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String letter = "Dear <|name|>, Thanks a lot";
+		String newLetter = letter.replace("<|name|>", "nitin");
+		System.out.println(newLetter);
+	}
+
+}
