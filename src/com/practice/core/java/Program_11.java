110e63b034e4df823ba92ea0a0571937ca2d6fef
Nitin Agrawal <nitinagrawalup@gmail.com>
2024-04-03 22:25:50 +0530

    Commit Program11

---
+++ src/com/practice/core/java/Program_11.java
@@ -0,0 +1,13 @@
+package com.practice.core.java;
+
+public class Program_11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "This is  nitin   agrawal,  what do you do?";
+		System.out.println("Double space is found at index :" + str.indexOf("  "));
+		System.out.println("Triple space is found at index :" + str.indexOf("   "));
+
+	}
+
+}
